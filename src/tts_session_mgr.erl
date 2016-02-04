185e54aa51c8453be63b60502bc7e5e60c6940d2
Bas Wegh <bas.wegh@kit.edu>
2016-02-04 12:46:04 +0000

    remove unneeded intermediat function

--- src/tts_session_mgr.erl
+++ src/tts_session_mgr.erl
@@ -32,7 +32,7 @@ new_session() ->
 
 -spec get_session(ID :: uuid:uuid()) -> {ok, pid()} | {error, not_found}.
 get_session(ID) ->
-    return_pid_or_error(lookup_session(ID)).
+    lookup_session_pid(ID).
 
 -spec session_closing(ID :: binary()) -> ok.
 session_closing(ID) ->
@@ -79,11 +79,6 @@ repeat_id_gen_if_needed(ok,ID,_State) ->
 repeat_id_gen_if_needed(_,_ID,State) ->
     get_unique_id(State).
 
-return_pid_or_error({ok, {_ID, Pid}}) ->
-    {ok, Pid};
-return_pid_or_error({error,_} = Error) ->
-    Error.
-
 
 %% 
 %% functions with data access
@@ -92,7 +87,7 @@ return_pid_or_error({error,_} = Error) ->
 add_new_session_entry(ID,_State) ->
    tts_data:sessions_create_new(ID). 
 
-lookup_session(ID) ->
+lookup_session_pid(ID) ->
    tts_data:sessions_get_pid(ID). 
 
 set_session_for_id(ID,Pid) ->
