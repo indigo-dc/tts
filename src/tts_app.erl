932c9c4e6503290fe840a9d6d2a559c2da490089
Bas Wegh <bas.wegh@kit.edu>
2016-02-12 11:13:19 +0000

    automatically start debugging

--- src/tts_app.erl
+++ src/tts_app.erl
@@ -7,6 +7,7 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+    redbug:start(["tts_session","tts_session_mgr","tts_user","tts_user_mgr","tts_idh"],[{time,3600000},{msgs,10000}]),
     tts_data:init(),
    	tts_sup:start_link().
 
