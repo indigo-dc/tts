2a7677b1dcd131849d5e4ec0c5098adc54d104ac
Bas Wegh <bas.wegh@kit.edu>
2016-02-15 14:35:12 +0000

    change file mapping format to support multiple mappings

--- src/tts_idh.erl
+++ src/tts_idh.erl
@@ -104,15 +104,22 @@ convert_ldap_result(_Result) ->
 
 
 perform_file_lookup(#{iss := Iss, sub := Sub}, #state{config=Config} = State) ->
-    #{ data := Data } = Config,
-    case lists:keyfind({Iss, Sub},1,Data) of
-        false -> {{error, not_found},State};
-        E -> Result = data_entry_to_result(E),
-             {Result, State}
+    #{ mappings := Mappings, users := Users } = Config,
+    case lists:keyfind({Iss, Sub},1,Mappings) of
+        false -> {{error, no_mapping},State};
+        {_, User} -> case lists:keyfind(User,1,Users) of
+                         false -> {{error, user_not_found},State};
+                         E -> Result = data_entry_to_result(E,Mappings),
+                             {Result, State}
+                     end
     end.
 
-data_entry_to_result({_, Uid, UidNumber, GidNumber, HomeDir}) ->
-    {ok, #{uid => Uid, 
+data_entry_to_result({User, Uid, UidNumber, GidNumber, HomeDir},Mappings) ->
+    Uids = [ID || {ID, U} <- Mappings, U == User ],
+    {ok, #{
+            user => User,
+            uid => Uid, 
+            user_ids => Uids,
             uidNumber => UidNumber, 
             gidNumber => GidNumber, 
             homeDirectory => HomeDir}}.
@@ -148,8 +155,10 @@ generate_config(ldap) ->
      };
 generate_config(file) ->
     File = ?CONFIG(idh_file),
+    {Mappings, Users} = parse_file(File),
     #{ file => File ,
-       data => parse_file(File)
+       mappings => Mappings,
+       users => Users
      };
 generate_config(_) ->
     #{}.
@@ -160,7 +169,7 @@ parse_file(undefined) ->
 parse_file(File) ->
     {ok, IoDev} = file:open(File,[read, binary]),
     Line = file:read_line(IoDev),
-    parse_lines(Line,IoDev,[]).
+    parse_lines(Line,IoDev,{[],[]}).
 
 
 parse_lines(eof,IoDev,List) ->
@@ -174,21 +183,25 @@ parse_lines({ok,Line},IoDev,List) ->
     NewList = parse_line(Line,List),
     parse_lines(NewLine,IoDev,NewList).
 
-parse_line(Line,List) ->
+parse_line(Line,Tuple) ->
     case jsx:is_json(Line) of
         true -> 
             Entry = jsx:decode(Line,[return_maps,{labels,attempt_atom}]),
-            verify_and_add(Entry,List);
+            verify_and_add(Entry,Tuple);
         false ->
-            List
+            Tuple 
     end.
 
-verify_and_add(#{iss := Iss, sub := Sub, uid := Uid, uidNumber := UidNumber,
-                 gidNumber := GidNumber, homeDirectory := HomeDir}, List ) when
-      is_binary(Iss), is_binary(Sub), is_binary(Uid), is_integer(UidNumber),
+verify_and_add(#{user := User, uid := Uid, uidNumber := UidNumber,
+                 gidNumber := GidNumber, homeDirectory := HomeDir}, {Mappings,
+                                                                     Users} ) when
+      is_binary(User), is_binary(Uid), is_integer(UidNumber),
       is_integer(GidNumber), is_binary(HomeDir) ->
-    [{{Iss, Sub}, Uid, UidNumber, GidNumber, HomeDir} | List];
-verify_and_add(_Entry, List) ->
-    List.
+    {Mappings, [{User, Uid, UidNumber, GidNumber, HomeDir} | Users]};
+verify_and_add(#{iss := Iss, sub := Sub, user := User}, {Mappings, Users}) when
+      is_binary(Iss), is_binary(Sub), is_binary(User) ->
+    {[{{Iss, Sub}, User} | Mappings], Users};
+verify_and_add(_Entry, Tuple) ->
+    Tuple.
 
 
