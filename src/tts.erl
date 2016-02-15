a2929087dee2b6231566b4895d5232d81c1fcb0f
Bas Wegh <bas.wegh@kit.edu>
2016-02-15 07:23:49 +0000

    add tts.erl with live debugging options

---
+++ src/tts.erl
@@ -0,0 +1,33 @@
+-module(tts). 
+
+
+-export([
+         start_debug/0,
+         start_debug/1,
+         start_debug/2,
+         stop_debug/0
+        ]).
+
+
+start_debug() ->
+    %debug these modules
+    ListOfModules = [ "tts_http", 
+                      "tts_user",
+                      "tts_user_mgr",
+                      "tts_session",
+                      "tts_session_mgr",
+                      "tts_idh"
+                    ],
+    start_debug(ListOfModules).
+
+start_debug(ListOfModules) ->
+    %debug for an hour or 10000 messages
+    Options = [{time,3600000},{msgs, 10000}],
+    start_debug(ListOfModules,Options).
+
+start_debug(ListOfModules,Options) ->
+    redbug:start(ListOfModules,Options).
+
+
+stop_debug() ->
+    redbug:stop().
