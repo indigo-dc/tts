8928177dfaf522a0e65d3a950f51ba4dc54215c7
Bas Wegh <bas.wegh@kit.edu>
2016-02-16 13:00:25 +0000

    return some hardcoded list

--- src/tts_plugin.erl
+++ src/tts_plugin.erl
@@ -1,7 +1,21 @@
 -module(tts_plugin).
 
--export([start/2]).
+-export([get_service_list_for_user/1]).
+%% -export([request_credential/1]).
+%% -export([delete_credential/1]).
+%% -export([security_incident/1]).
 
 
-start(_Type, _Config) ->
-    ok.
+get_service_list_for_user(_Uid) ->
+    SshService = #{ id => <<"service1">>,
+                    description => <<"a simple ssh service">>,
+                    host => <<"tts.data.kit.edu">>,
+                    type => <<"ssh">>
+                  },
+    S3Service = #{ id => <<"service2">>,
+                    description => <<"a simple S3 service">>,
+                    host => <<"tts.data.kit.edu">>,
+                    type => <<"S3">>
+                  },
+    ServiceList = [SshService, S3Service],
+    {ok,ServiceList}.
