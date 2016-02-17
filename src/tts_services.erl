c09082a7c563dd4c6e0e091fe70f5a23dba9c5eb
Bas Wegh <bas.wegh@kit.edu>
2016-02-17 09:35:18 +0000

    implement some stubs

--- src/tts_services.erl
+++ src/tts_services.erl
@@ -1,12 +1,13 @@
 -module(tts_services).
 
 -export([get_list_for_user/1]).
-%% -export([request_credential/1]).
-%% -export([revoke_credential/1]).
-%% -export([security_incident/1]).
+-export([get_credential_list/1]).
+-export([request_credential/4]).
+-export([revoke_credential/1]).
+-export([security_incident/1]).
 
 
-get_list_for_user(_Uid) ->
+get_list_for_user(_UserId) ->
     SshService = #{ id => <<"service1">>,
                     description => <<"a simple ssh service">>,
                     host => <<"tts.data.kit.edu">>,
@@ -19,3 +20,24 @@ get_list_for_user(_Uid) ->
                   },
     ServiceList = [SshService, S3Service],
     {ok,ServiceList}.
+
+
+get_credential_list(_UserId) ->
+    {ok,[]}.
+
+request_credential(ServiceId,UserId,Token,Params) ->
+    request_if_allowed(is_allowed(UserId, ServiceId, Token),ServiceId,UserId,Token,Params).
+
+revoke_credential(_CredentialId) ->
+    ok.
+    
+security_incident(_CredentialId) ->
+    ok.
+
+request_if_allowed(false, _ServiceId, _UserId, _Token, _Params) ->
+    {error, not_allowed};
+request_if_allowed(true, _ServiceId, _UserId, _Token, _Params) ->
+    {ok, #{}}.
+
+is_allowed(_UserId, _ServiceId, _Token) ->
+    true.
