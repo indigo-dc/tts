57108a1df2c265b41da32515ad0a6f054d81a1bd
Bas Wegh <bas.wegh@kit.edu>
2016-03-01 07:47:20 +0000

    cleanup service management

--- src/tts_services.erl
+++ src/tts_services.erl
@@ -1,49 +1,21 @@
 -module(tts_services).
 
+-export([get_service_list/0]).
 -export([get_service_list/1]).
--export([get_credential_list/1]).
--export([request_credential/3]).
--export([request_credential/4]).
--export([revoke_credential/1]).
--export([security_incident/1]).
+-export([get_service_info/1]).
+-export([add_service/2]).
 
 
-get_service_list(_UserId) ->
-    SshService = #{ id => <<"service1">>,
-                    description => <<"a simple ssh service">>,
-                    host => <<"tts.data.kit.edu">>,
-                    type => <<"ssh">>
-                  },
-    S3Service = #{ id => <<"service2">>,
-                    description => <<"a simple S3 service">>,
-                    host => <<"tts.data.kit.edu">>,
-                    type => <<"S3">>
-                  },
-    ServiceList = [SshService, S3Service],
-    {ok,ServiceList}.
-
-
-get_credential_list(_UserId) ->
-    % TODO: implement, should be related to tts_data
+get_service_list() ->
     {ok,[]}.
 
-request_credential(ServiceId,UserId,Token) ->
-    request_credential(ServiceId,UserId,Token,[]).
+get_service_list(_UserId) ->
+    %TODO: implement a whitelist per service
+    tts_data:service_get_list().
 
-request_credential(ServiceId,UserId,Token,Params) ->
-    request_if_allowed(is_allowed(UserId, ServiceId, Token),ServiceId,UserId,Token,Params).
+get_service_info(ServiceId) ->
+    tts_data:service_get(ServiceId).
 
-revoke_credential(_CredentialId) ->
-    ok.
-    
-security_incident(_CredentialId) ->
-    ok.
+add_service(ServiceId,ServiceInfo) ->
+    tts_data:service_add(ServiceId,ServiceInfo).
 
-request_if_allowed(false, _ServiceId, _UserInfo, _Token, _Params) ->
-    {error, not_allowed};
-request_if_allowed(true, ServiceId, UserInfo, Token, Params) ->
-    {ok, Pid} = tts_service_sup:start_worker(),
-    tts_service_worker:perform_request(ServiceId,UserInfo,Token,Params,Pid).
-
-is_allowed(_UserId, _ServiceId, _Token) ->
-    true.
