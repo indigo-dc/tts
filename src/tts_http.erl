f99a59e6efb320df7f675e3be094a0c5a56e001a
Bas Wegh <bas.wegh@kit.edu>
2016-03-17 15:45:27 +0000

    transform the token map into a string

--- src/tts_http.erl
+++ src/tts_http.erl
@@ -121,7 +121,8 @@ show_user_page(Session,Credential,Log) ->
     Params = case ?DEBUG_MODE of
                  true -> 
                      {ok, Token} = tts_session:get_token(Session),
-                     [{token,Token} |BaseParams];
+                     TokenText = io_lib:format("~p",[Token]),
+                     [{token,TokenText} |BaseParams];
                  _ -> BaseParams
              end,
     {ok, Body} = tts_user_dtl:render(Params),
