26984f6af97abbf023a432d586506a325dd081fa
Bas Wegh <bas.wegh@kit.edu>
2016-05-09 09:05:14 +0100

    adjust tests

--- test/tts_session_test.erl
+++ test/tts_session_test.erl
@@ -4,28 +4,37 @@
 -define(ID,<<"JustSomeID">>).
 
 start_stop_id_test() ->
+    MeckModules = [tts_session_mgr],
+    test_util:meck_new(MeckModules),
+    ok = meck:expect(tts_session_mgr,session_terminating, fun(_) -> ok end),
     {ok, Pid} = tts_session:start_link(?ID),
     {ok, ?ID} = tts_session:get_id(Pid),
     ok = tts_session:close(Pid),
-    ok = test_util:wait_for_process_to_die(Pid,100).
+    ok = test_util:wait_for_process_to_die(Pid,100),
+    ok = test_util:meck_done(MeckModules).
 
 get_set_max_age_test() ->
+    MeckModules = [tts_session_mgr],
+    test_util:meck_new(MeckModules),
     {ok, Pid} = tts_session:start_link(?ID),
     %set up the mocking of tts_session_mgr
     CloseFun = fun(ID) ->
                        ID = ?ID,
                        tts_session:close(Pid)
                end,
-    ok = meck:new(tts_session_mgr),
+    ok = meck:expect(tts_session_mgr,session_terminating, fun(_) -> ok end),
     ok = meck:expect(tts_session_mgr,session_wants_to_close, CloseFun),
     {ok, _} = tts_session:get_max_age(Pid),
     ok = tts_session:set_max_age(1000,Pid),
     {ok, 1000} = tts_session:get_max_age(Pid),
     ok = tts_session:set_max_age(1,Pid),
     ok = test_util:wait_for_process_to_die(Pid,100),
-    ok = meck:unload(tts_session_mgr).
+    ok = test_util:meck_done(MeckModules).
 
 oidc_test() ->
+    MeckModules = [tts_session_mgr],
+    test_util:meck_new(MeckModules),
+    ok = meck:expect(tts_session_mgr, session_terminating, fun(_) -> ok end),
     {ok, Pid} = tts_session:start_link(?ID),
     {ok, OidcState} = tts_session:get_oidc_state(Pid),
     {ok, OidcNonce} = tts_session:get_oidc_nonce(Pid),
@@ -38,9 +47,14 @@ oidc_test() ->
     ok = tts_session:clear_oidc_state_nonce(Pid),
     {ok, cleared} = tts_session:get_oidc_state(Pid),
     {ok, cleared} = tts_session:get_oidc_nonce(Pid),
-    ok = tts_session:close(Pid).
+    ok = tts_session:close(Pid),
+    ok = test_util:wait_for_process_to_die(Pid,100),
+    ok = test_util:meck_done(MeckModules).
 
 token_login_test() ->
+    MeckModules = [tts_session_mgr],
+    test_util:meck_new(MeckModules),
+    ok = meck:expect(tts_session_mgr, session_terminating, fun(_) -> ok end),
     {ok, Pid} = tts_session:start_link(?ID),
     Subject = "foo",
     Issuer = "https://bar.com",
@@ -54,28 +68,46 @@ token_login_test() ->
     {ok, Issuer, Subject} = tts_session:get_iss_sub(Pid),
     true = tts_session:is_logged_in(Pid),
     ignored = tts_session:set_token(BadToken, Pid),
-    ok = tts_session:close(Pid).
+    ok = tts_session:close(Pid),
+    ok = test_util:wait_for_process_to_die(Pid,100),
+    ok = test_util:meck_done(MeckModules).
+
 
 
 redirect_test() ->
+    MeckModules = [tts_session_mgr],
+    test_util:meck_new(MeckModules),
+    ok = meck:expect(tts_session_mgr, session_terminating, fun(_) -> ok end),
     Redirect = <<"https://localhost/back">>,
     BadRedirect = "https://localhost/back",
     {ok, Pid} = tts_session:start_link(?ID),
     ok = tts_session:set_used_redirect(Redirect, Pid),
     {ok,Redirect} = tts_session:get_used_redirect(Pid),
     ignored = tts_session:set_used_redirect(BadRedirect, Pid),
-    ok = tts_session:close(Pid).
+    ok = tts_session:close(Pid),
+    ok = test_util:wait_for_process_to_die(Pid,100),
+    ok = test_util:meck_done(MeckModules).
+
 
 
 oidc_provider_test() ->
+    MeckModules = [tts_session_mgr],
+    test_util:meck_new(MeckModules),
+    ok = meck:expect(tts_session_mgr,session_terminating, fun(_) -> ok end),
     {ok, Pid} = tts_session:start_link(?ID),
     OidcId = <<"SomeOpenIdProviderId">>,
     ok = tts_session:set_oidc_provider(OidcId, Pid),
     {ok, OidcId} = tts_session:get_oidc_provider(Pid),
-    ok = tts_session:close(Pid).
+    ok = tts_session:close(Pid),
+    ok = test_util:wait_for_process_to_die(Pid,100),
+    ok = test_util:meck_done(MeckModules).
+
 
 
 same_ua_ip_test() ->
+    MeckModules = [tts_session_mgr],
+    test_util:meck_new(MeckModules),
+    ok = meck:expect(tts_session_mgr,session_terminating, fun(_) -> ok end),
     IP = {123,123,123,123},
     UA = <<"some browser">>,
     {ok, Pid} = tts_session:start_link(?ID),
@@ -85,11 +117,20 @@ same_ua_ip_test() ->
     true = tts_session:is_same_ip(IP,Pid),
     false = tts_session:is_same_ip(UA,Pid),
     true = tts_session:is_same_ip(IP,Pid),
-    ok = tts_session:close(Pid).
+    ok = tts_session:close(Pid),
+    ok = test_util:wait_for_process_to_die(Pid,100),
+    ok = test_util:meck_done(MeckModules).
+
 
 garbage_test() ->
+    MeckModules = [tts_session_mgr],
+    test_util:meck_new(MeckModules),
+    ok = meck:expect(tts_session_mgr,session_terminating, fun(_) -> ok end),
     {ok, Pid} = tts_session:start_link(?ID),
     Pid ! <<"some data">>,
     ok = gen_server:cast(Pid,some_cast),
     still_alive = test_util:wait_for_process_to_die(Pid,5),
-    ok = tts_session:close(Pid).
+    ok = tts_session:close(Pid),
+    ok = test_util:wait_for_process_to_die(Pid,100),
+    ok = test_util:meck_done(MeckModules).
+
