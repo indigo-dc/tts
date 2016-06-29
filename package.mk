6db080693c3937f27f1d8f0f2c72abfd48569aba
Bas Wegh <bas.wegh@kit.edu>
2016-06-29 08:44:50 +0100

    delete .git and .gitignore in src package

--- package.mk
+++ package.mk
@@ -87,6 +87,8 @@ build_clean_dir = cd distdir/$(CLONEDIR) && \
                       cd $${dep} && \
                            mkdir -p ../../../../../$(PKG_ID)/_build/default/$${dep}/priv && \
                            printf "`git describe --long --tags 2>/dev/null || git rev-parse HEAD`" > ../../../../../$(PKG_ID)/_build/default/$${dep}/priv/vsn.git && \
+                           rm -rf ../../../../../$(PKG_ID)/_build/default/$${dep}/.git && \
+                           rm -f ../../../../../$(PKG_ID)/_build/default/$${dep}/.gitignore && \
                            cd ../..; \
                   done && \
 				  cd ../..
