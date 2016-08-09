eec71844aedeb591593fa2973edc3ae2d210c331
Bas Wegh <bas.wegh@kit.edu>
2016-08-09 14:25:02 +0100

    add revocation to x509 plugin

--- tts_config/sample/tts/scripts/x509.py
+++ tts_config/sample/tts/scripts/x509.py
@@ -23,6 +23,9 @@ database          = \$dir/index.txt
 certificate       = \$dir/certs/cacert.pem
 serial            = \$dir/serial
 private_key       = \$dir/private/cakey.pem
+crldir            = \$dir/crl
+crl               = \$crldir/crl.pem
+crlnumber         = \$crldir/crlnumber
 default_days      = 365
 default_crl_days  = 30
 default_md        = sha1
@@ -47,12 +50,17 @@ def create_cert(Subject, Issuer):
     Serial = read_serial()
     return issue_certificate(Subject, Issuer, Serial)
 
+def revoke_cert(Serial):
+    init_ca_if_needed()
+    return revoke_certificate(Serial)
+
 def issue_certificate(Subject, Issuer, Serial):
     AbsBase = CA_ABS_BASE
     IssuerDomain = string.rsplit(Issuer, "/", 1)[-1]
     Subject = CERT_SUBJECT%(Subject, IssuerDomain, Serial)
     Password = id_generator(32)
     CAPassFile = "%s/private/pass"%(AbsBase)
+    CACertFile = "%s/certs/cacert.pem"%(AbsBase)
     CertFile = "%s/certs/usercert_%s.pem"%(AbsBase, Serial)
     CsrFile = "%s/users/csr/userreq_%s.pem"%(AbsBase, Serial)
     KeyFile = "%s/users/private/userkey_%s.pem"%(AbsBase, Serial)
@@ -73,21 +81,35 @@ def issue_certificate(Subject, Issuer, Serial):
     if os.system(Cmd) != 0:
         return json.dumps({'error':'sign_failed'})
     Cert = get_file_content(CertFile)
+    CACert = get_file_content(CACertFile)
     PrivKey = get_file_content(KeyFile)
     Cmd = "rm %s %s"%(PassFile, KeyFile)
     if os.system(Cmd) != 0:
         return json.dumps({'error':'purge_files'})
     CertObj = {'name':'Certificate', 'type':'textfile', 'value':Cert, 'rows':'30', 'cols':'64'}
-    PrivKeyObj = {'name':'Private Key', 'type':'textfile', 'value':PrivKey, 'rows':'30', 'cols':'64'}
+    PrivKeyObj = {'name':'Private Key', 'type':'textfile', 'value':PrivKey, 'rows':'21', 'cols':'64'}
     PasswdObj = {'name':'Passphrase (for Private Key)', 'type':'text', 'value':Password}
-    Credential = [CertObj, PrivKeyObj, PasswdObj]
+    CACertObj = {'name':'CA Certificate', 'type':'textfile', 'value':CACert, 'rows':'21', 'cols':'64'}
+    Credential = [CertObj, PrivKeyObj, PasswdObj, CACertObj]
     return json.dumps({'credential':Credential, 'state':Serial})
 
 
-def revoke_cert(SerialNumber):
-    init_ca_if_needed()
-    #just return okay for now, will add revocation later
+def revoke_certificate(Serial):
+    AbsBase = CA_ABS_BASE
+    LogFile = "%s/users/private/userlog_%s"%(AbsBase, Serial)
+    CertFile = "%s/certs/usercert_%s.pem"%(AbsBase, Serial)
+    ConfFile = "%s/openssl.conf"%(AbsBase)
+    CAPassFile = "%s/private/pass"%(AbsBase)
+    CrlFile = "%s/crl/crl.pem"%(AbsBase)
+    ConfigPass = "-config %s -passin file:%s"%(ConfFile, CAPassFile)
+    Cmd = "openssl ca %s -revoke %s >> %s 2>&1"%(ConfigPass, CertFile, LogFile) 
+    if os.system(Cmd) != 0:
+        return json.dumps({'error':'revoke'})
+    Cmd = "openssl ca -gencrl %s -out %s >> %s 2>&1"%(ConfigPass, CrlFile, LogFile) 
+    if os.system(Cmd) != 0:
+        return json.dumps({'error':'crl'})
     return json.dumps({'result':'ok'})
+
 
 def init_ca_if_needed():
     if not os.path.isdir(CA_ABS_BASE):
@@ -108,12 +130,16 @@ def init_ca():
     os.mkdir("%s/users"%(AbsBase))
     os.mkdir("%s/users/csr"%(AbsBase))
     os.mkdir("%s/users/private"%(AbsBase))
+    os.mkdir("%s/crl"%(AbsBase))
 
     LogFile = "%s/private/ca.log"%(AbsBase)
     Cmd = "touch %s/index.txt > /dev/null"%(AbsBase)
     if os.system(Cmd) != 0:
         return json.dumps({'error':'touch_failed'})
     Cmd = "echo \"01\" > %s/serial"%(AbsBase)
+    if os.system(Cmd) != 0:
+        return json.dumps({'error':'serial_failed'})
+    Cmd = "echo \"01\" > %s/crl/crlnumber"%(AbsBase)
     if os.system(Cmd) != 0:
         return json.dumps({'error':'serial_failed'})
     Config = OPENSSL_CONF%(AbsBase)
