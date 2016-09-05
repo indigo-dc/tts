08571e07bf16fa625db2d84fc08c23d6411801c5
Bas Wegh <bas.wegh@kit.edu>
2016-09-05 15:30:17 +0200

    use tts for login

--- src/tts_oidc_client.erl
+++ src/tts_oidc_client.erl
@@ -7,37 +7,28 @@
          login_failed/2
         ]).
 
+
 -define(COOKIE, <<"tts_session">>).
 
-login_succeeded(#{id := #{claims := #{ sub := Subject, iss := Issuer}}} = TokenMap) ->
-    {ok, SessPid} = tts_session_mgr:new_session(),
-    try
-        {ok, UserInfo} = tts_user_cache:get_user_info(Issuer, Subject),
-        {ok, SessId} = tts_session:get_id(SessPid),
-        ok = tts_session:set_token(TokenMap, SessPid),
-        ok = tts_session:set_user_info(UserInfo, SessPid),
-        {ok, MaxAge} = tts_session:get_max_age(SessPid),
-        lager:debug("login success"),
-        Opts = create_cookie_opts(MaxAge), 
-        {ok, [{redirect, ?CONFIG(ep_user)}, {cookie, ?COOKIE, SessId, Opts}]}
-    catch _:_ ->
+login_succeeded(TokenMap) ->
+    case tts:login_with_oidcc(TokenMap) of
+        {ok, #{session_id := SessId, session_pid := SessPid}} ->
+            {ok, MaxAge} = tts_session:get_max_age(SessPid),
+            Opts = create_cookie_opts(MaxAge),
+            {ok, [{redirect, ?CONFIG(ep_main)},
+                  {cookie, ?COOKIE, SessId, Opts}]};
+        {error, _} ->
             %% TODO:
-            %% show an error on the login page 
-            tts_session:close(SessPid),
-            lager:debug("login internal fail"),
+            %% show an error on the login page
             {ok, [{redirect, ?CONFIG(ep_main)}]}
-    end;
-login_succeeded(_BadToken) -> 
-    %% TODO:
-    %% show an error on the login page 
-    lager:debug("bad token"),
-    {ok, [{redirect, ?CONFIG(ep_main)}]}.
+    end.
 
 login_failed(_Reason, _Description) ->
     %% TODO:
-    %% show an error on the login page 
+    %% show an error on the login page
     lager:debug("login failed"),
     {ok, [{redirect, ?CONFIG(ep_main)}]}.
+
 
 create_cookie_opts(MaxAge) ->
     BasicOpts = [ {http_only, true}, {max_age, MaxAge}, {path, <<"/">>}],
