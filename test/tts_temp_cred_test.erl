2ed3d87d6b24fbb893dea2e2d553059ba6361d15
Bas Wegh <bas.wegh@kit.edu>
2016-09-05 15:41:01 +0200

    rename rest_cred to temp_cred in test

--- test/tts_temp_cred_test.erl
+++ test/tts_temp_cred_test.erl
@@ -1,23 +1,23 @@
--module(tts_rest_cred_test).
+-module(tts_temp_cred_test).
 -include_lib("eunit/include/eunit.hrl").
 
 start_stop_test() ->
-    {ok, Pid} = tts_rest_cred:start_link(),
-    ok = tts_rest_cred:stop(),
+    {ok, Pid} = tts_temp_cred:start_link(),
+    ok = tts_temp_cred:stop(),
     ok = test_util:wait_for_process_to_die(Pid,300),
     ok.
 
 add_get_test() ->
-    {ok, Pid} = tts_rest_cred:start_link(),
+    {ok, Pid} = tts_temp_cred:start_link(),
 
-    UserId = <<"user1">>,
-    {ok, Id} = tts_rest_cred:add_cred(#{}, UserId),
+    UserInfo = #{ site => #{ uid => <<"user1">>}},
+    {ok, Id} = tts_temp_cred:add_cred(#{}, UserInfo),
 
-    {ok, #{}} = tts_rest_cred:get_cred(Id, UserId),
-    {error, _} = tts_rest_cred:get_cred(Id, UserId),
+    {ok, #{}} = tts_temp_cred:get_cred(Id, UserInfo),
+    {error, _} = tts_temp_cred:get_cred(Id, UserInfo),
 
 
-    ok = tts_rest_cred:stop(),
+    ok = tts_temp_cred:stop(),
     ok = test_util:wait_for_process_to_die(Pid,300),
     ok.
 
