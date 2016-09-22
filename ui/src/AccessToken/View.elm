1d4f52d11044471d7994c84dcad02f9094791595
Bas Wegh <bas.wegh@kit.edu>
2016-09-22 11:29:26 +0200

    show access token

---
+++ ui/src/AccessToken/View.elm
@@ -0,0 +1,33 @@
+module AccessToken.View exposing (..)
+
+import AccessToken.Model as AccessToken exposing (Model)
+import Dialog
+import Html exposing (Html, text, textarea)
+import Html.Attributes exposing (class, rows, readonly)
+import Messages exposing (Msg)
+
+
+view : AccessToken.Model -> Html Msg
+view at =
+    let
+        config =
+            if at.token == "" then
+                Nothing
+            else
+                Just
+                    { closeMessage = Just Messages.HideAccessToken
+                    , containerClass = Nothing
+                    , header = Just (text "Your Access Token")
+                    , body =
+                        Just
+                            (textarea
+                                [ class "form-control"
+                                , rows 10
+                                , readonly True
+                                ]
+                                [ text at.token ]
+                            )
+                    , footer = Nothing
+                    }
+    in
+        Dialog.view (config)
