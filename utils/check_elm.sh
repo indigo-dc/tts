1e6db969ee5e941078d284f1ae2e50051ecf46ac
Bas Wegh <bas.wegh@kit.edu>
2016-09-22 11:30:09 +0200

    use closure-compiler for optimization

--- utils/check_elm.sh
+++ utils/check_elm.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 ELM_MAKE=`which elm-make`
+CLOSURE_COMPILER=`which closure-compiler`
 if [ "x$ELM_MAKE" == "x" ] ; then
     echo "***"
     echo " no Elm installed on the system"
     echo " please install Elm Version 0.17.0 "
     echo " "
     exit 1
+elif [ "x$CLOSURE_COMPILER" == "x" ] ; then
+    echo "***"
+    echo " no closure-compiler installed on the system"
+    echo " please install the closure compiler "
+    echo " "
+    exit 1
 else
-    echo "Elm found on the system"
+    echo "Elm and closure-compiler found on the system"
     exit 0
 fi
