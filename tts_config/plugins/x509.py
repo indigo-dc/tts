a3e48e6740366e39a466d98b975278feeb83bb25
Bas Wegh <bas.wegh@kit.edu>
2016-09-28 16:26:50 +0200

    add shorten function to contain subject and issuer in CN

--- tts_config/plugins/x509.py
+++ tts_config/plugins/x509.py
@@ -11,10 +11,10 @@ import random
 
 CA_BASE="{{platform_data_dir}}/tts_ca"
 CA_SUBJECT="/C=EU/O=INDIGO/OU=TTS/CN=TTS-CA"
-CERT_SUBJECT="/C=EU/O=INDIGO/OU=TTS/CN=%s"
+CERT_SUBJECT="/C=EU/O=INDIGO/OU=TTS/CN=%s@%s"
 OPENSSL_CONF = """
 [ ca ]
-default_ca = CA_default 
+default_ca = CA_default
 
 [ CA_default ]
 dir               = %s
@@ -45,6 +45,10 @@ keyUsage                =digitalSignature, keyEncipherment
 """
 CA_ABS_BASE=os.path.abspath(os.path.expanduser(CA_BASE))
 
+ISSUER_MAPPING=""" { "https://iam-test.indigo-datacloud.eu/":"indigo-iam-test",
+                     "https://accounts.google.com":"google"
+                   } """
+
 def create_cert(Subject, Issuer):
     init_ca_if_needed()
     Serial = read_serial()
@@ -55,11 +59,15 @@ def revoke_cert(Serial):
     return revoke_certificate(Serial)
 
 def issue_certificate(Subject, Issuer, Serial):
+    ShortIss = shorten_issuer(Issuer)
+    if ShortIss == None:
+        return json.dumps({'error':'unknown issuer'})
+
     AbsBase = CA_ABS_BASE
     Issuer = string.rstrip(Issuer, "/")
-    CertSubject = CERT_SUBJECT%(Serial)
     Password = id_generator(32)
-    AltSub = "subjectAltName = URI:%s/%s/%s"%(Issuer, Subject, Serial)
+    CertSubject = CERT_SUBJECT%(Subject, ShortIss)
+    AltSub = "subjectAltName = URI:%s/%s"%(Issuer, Subject)
     CAPassFile = "%s/private/pass"%(AbsBase)
     CACertFile = "%s/certs/cacert.pem"%(AbsBase)
     CertFile = "%s/certs/usercert_%s.pem"%(AbsBase, Serial)
@@ -70,11 +78,12 @@ def issue_certificate(Subject, Issuer, Serial):
     ConfFile = "%s/openssl.conf"%(AbsBase)
     LogFile = "%s/users/private/userlog_%s"%(AbsBase, Serial)
     Cmd = "echo -n \"%s\" > %s"%(Password, PassFile)
+
     if os.system(Cmd) != 0:
         return json.dumps({'error':'userpass_failed'})
 
-    Cmd = "openssl req -newkey rsa:1024 -keyout %s -sha256 -out %s -subj \"%s\" -passout file:%s >> %s 2>&1"%(KeyFile, CsrFile, CertSubject, PassFile, LogFile) 
-    Log = "echo %s > %s"%(Cmd, LogFile) 
+    Cmd = "openssl req -newkey rsa:1024 -keyout %s -sha256 -out %s -subj \"%s\" -passout file:%s >> %s 2>&1"%(KeyFile, CsrFile, CertSubject, PassFile, LogFile)
+    Log = "echo %s > %s"%(Cmd, LogFile)
     os.system(Log)
     if os.system(Cmd) != 0:
         return json.dumps({'error':'csr_failed'})
@@ -88,7 +97,7 @@ def issue_certificate(Subject, Issuer, Serial):
         return json.dumps({'error':'conf_update_failed'})
 
     Cmd = "openssl ca -batch -config %s -policy policy_anything -extensions usr_cert -out %s -passin file:%s -infiles %s >> %s 2>&1"%(TmpConfFile, CertFile, CAPassFile, CsrFile, LogFile)
-    Log = "echo %s >> %s"%(Cmd, LogFile) 
+    Log = "echo %s >> %s"%(Cmd, LogFile)
     os.system(Log)
     if os.system(Cmd) != 0:
         return json.dumps({'error':'sign_failed'})
@@ -106,6 +115,12 @@ def issue_certificate(Subject, Issuer, Serial):
     return json.dumps({'credential':Credential, 'state':Serial})
 
 
+def shorten_issuer(Issuer):
+    IssuerDict = json.loads(ISSUER_MAPPING.replace("\n",""))
+    if Issuer in IssuerDict:
+        return IssuerDict[Issuer]
+    return None
+
 def revoke_certificate(Serial):
     AbsBase = CA_ABS_BASE
     LogFile = "%s/users/private/userlog_%s"%(AbsBase, Serial)
@@ -114,10 +129,10 @@ def revoke_certificate(Serial):
     CAPassFile = "%s/private/pass"%(AbsBase)
     CrlFile = "%s/crl/crl.pem"%(AbsBase)
     ConfigPass = "-config %s -passin file:%s"%(ConfFile, CAPassFile)
-    Cmd = "openssl ca %s -revoke %s >> %s 2>&1"%(ConfigPass, CertFile, LogFile) 
+    Cmd = "openssl ca %s -revoke %s >> %s 2>&1"%(ConfigPass, CertFile, LogFile)
     if os.system(Cmd) != 0:
         return json.dumps({'error':'revoke'})
-    Cmd = "openssl ca -gencrl %s -out %s >> %s 2>&1"%(ConfigPass, CrlFile, LogFile) 
+    Cmd = "openssl ca -gencrl %s -out %s >> %s 2>&1"%(ConfigPass, CrlFile, LogFile)
     if os.system(Cmd) != 0:
         return json.dumps({'error':'crl'})
     return json.dumps({'result':'ok'})
@@ -126,12 +141,12 @@ def revoke_certificate(Serial):
 def init_ca_if_needed():
     if not os.path.isdir(CA_ABS_BASE):
         init_ca()
-        
+
 def read_serial():
     SerialFile = "%s/serial"%(CA_ABS_BASE)
     Serial = get_file_content(SerialFile).rstrip('\n')
     return Serial
-    
+
 
 def init_ca():
     AbsBase = CA_ABS_BASE
@@ -171,7 +186,7 @@ def get_file_content(File):
     Content = fo.read()
     fo.close()
     return Content
-    
+
 def id_generator(size=16, chars=string.ascii_uppercase + string.digits+string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
 
@@ -193,37 +208,37 @@ def main():
 
             # information coming from the site
             # uid - the username
-            # uidNumber - the uid of the user 
-            # gidNumber - the gid of the primary group of the user 
-            # homeDirectory - the home directory of the user 
+            # uidNumber - the uid of the user
+            # gidNumber - the gid of the primary group of the user
+            # homeDirectory - the home directory of the user
             # UserName = Site['uid']
             # Uid = Site['uidNumber']
             # Gid = Site['gidNumber']
             # HomeDir = Site['homeDirectory']
 
-            # information coming from the openid provider 
-            # which information are available depends on the 
+            # information coming from the openid provider
+            # which information are available depends on the
             # OpenId Connect provider
-            # 
+            #
             # iss - the issuer
-            # sub - the subject 
-            # name - the full name of the user 
-            # email - the email of the user 
+            # sub - the subject
+            # name - the full name of the user
+            # email - the email of the user
             #
             # IAM also provides
             # groups - a list of groups each consisting of
-            #    id - uuid of the group 
-            #    name - readable name of the group 
-            # organisation_name - name of the organisation, indigo_dc 
-            # preferred_username - if possible create accounts with this name 
+            #    id - uuid of the group
+            #    name - readable name of the group
+            # organisation_name - name of the organisation, indigo_dc
+            # preferred_username - if possible create accounts with this name
             Issuer = Oidc['iss']
             Subject = Oidc['sub']
             # OidcUserName = Oidc['preferred_username']
 
             if Action == "request":
-                print create_cert(Subject, Issuer) 
+                print create_cert(Subject, Issuer)
             elif Action == "revoke":
-                print revoke_cert(State) 
+                print revoke_cert(State)
             else:
                 print json.dumps({"error":"unknown_action", "details":Action})
         else:
