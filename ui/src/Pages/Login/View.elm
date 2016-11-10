bd528cd3174dd197c4506422962d8b093606d025
Bas Wegh <bas.wegh@kit.edu>
2016-11-10 13:50:16 +0100

    add message when login is impossible, fix #199

--- ui/src/Pages/Login/View.elm
+++ ui/src/Pages/Login/View.elm
@@ -1,7 +1,7 @@
 module Pages.Login.View exposing (..)
 
 import Html exposing (Html, div, text, form, select, option, button, span)
-import Html.Attributes exposing (disabled, action, class, method, id, name, value, type')
+import Html.Attributes exposing (hidden, disabled, action, class, method, id, name, value, type')
 import ProviderList.Model as ProviderList exposing (Model)
 import ProviderList.View as ProviderList exposing (view)
 
@@ -14,23 +14,34 @@ type alias ViewContext =
 
 view : ViewContext -> Html msg
 view context =
-    div []
-        [ text "Please select your OpenId Connect Provider"
-        , div [ class "form-group" ]
-            [ form [ method "post", action context.path ]
-                [ div [ class "input-group" ]
-                    [ select [ id "provider", name "provider", class "form-control" ]
-                        (ProviderList.view context.providerList)
-                    , span [ class "input-group-btn" ]
-                        [ button
-                            [ type' "submit"
-                            , class "btn btn-primary"
-                            , disabled (not (ProviderList.hasReadyEntries context.providerList))
-                            ]
-                            [ text "Login"
+    let
+        hideLogin =
+            List.isEmpty context.providerList.provider
+
+        hideLoginImpossible =
+            not hideLogin
+    in
+        div []
+            [ div [ hidden hideLogin ]
+                [ text "Please select your OpenId Connect Provider"
+                , div [ class "form-group" ]
+                    [ form [ method "post", action context.path ]
+                        [ div [ class "input-group" ]
+                            [ select [ id "provider", name "provider", class "form-control" ]
+                                (ProviderList.view context.providerList)
+                            , span [ class "input-group-btn" ]
+                                [ button
+                                    [ type' "submit"
+                                    , class "btn btn-primary"
+                                    , disabled (not (ProviderList.hasReadyEntries context.providerList))
+                                    ]
+                                    [ text "Login"
+                                    ]
+                                ]
                             ]
                         ]
                     ]
                 ]
+            , div [ hidden hideLoginImpossible ]
+                [ text "Login is disabled as no OpenId Connect provider is configured" ]
             ]
-        ]
