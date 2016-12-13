a9b55ecbc2b2f11d55a6ff7e5a2fbc19a55e3bcb
Bas Wegh <bas.wegh@kit.edu>
2016-12-13 11:42:40 +0100

    fix issue with environment, part of #217

--- src/tts_init.erl
+++ src/tts_init.erl
@@ -117,8 +117,8 @@ add_openid_provider([], _) ->
 add_services() ->
     %% copy the version into the config
     %% only using env values, so everything can be tested
-    Vsn = application:get_key(tts, vsn),
-    application:set_env(tts, vsn, Vsn),
+    {ok, Vsn} = application:get_key(tts, vsn),
+    ok = application:set_env(tts, vsn, Vsn),
     ServiceList = ?CONFIG(service_list),
     ok = add_services(ServiceList),
     ok.
