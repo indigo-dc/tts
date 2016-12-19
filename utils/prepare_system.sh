aa82d136beb5ebe86507a856a788eb125d070a4b
Bas Wegh <bas.wegh@kit.edu>
2016-12-19 09:24:34 +0100

    unify system preparation

---
+++ utils/prepare_system.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+DISTRIBUTION_NAME=`cat /etc/os-release | grep PRETTY_NAME`
+CURRENT_DIR=`pwd`
+cd `dirname $0`
+UTILS_DIR=`pwd`
+
+DISTRIBUTION="unknown"
+case "$DISTRIBUTION_NAME" in
+    *Debian*)
+        DISTRIBUTION="debian"
+        ;;
+    *Ubuntu*)
+        DISTRIBUTION="ubuntu"
+        ;;
+    *CentOS*)
+        DISTRIBUTION="centos"
+        ;;
+esac
+echo "preparing the system ..."
+echo "distribution: $DISTRIBUTION"
+echo "utils dir: $UTILS_DIR"
+echo "current dir: $CURRENT_DIR"
+echo " "
+echo "installing needed packages"
+if [ "$DISTRIBUTION" = "unknown" ]; then
+    echo "ERROR: unknown distribution"
+    exit 1
+fi
+
+echo "*** INSTALLING PACKAGES ***"
+cd $UTILS_DIR
+case "$DISTRIBUTION" in
+    debian)
+        ./debian_install_packages.sh
+        ;;
+    ubuntu)
+        ./ubuntu_install_packages.sh
+        ;;
+    centos)
+        ./centos_install_packages.sh
+        ;;
+esac
+
+echo "*** BUILDING AND INSTALLING ERLANG ***"
+cd $UTILS_DIR
+./build_install_erlang.sh
+
+echo "*** BUILDING AND INSTALLING CUTTLEFISH ***"
+cd $UTILS_DIR
+./build_install_cuttlefish.sh
+
+cd $CURRENT_DIR
+echo "done"
