fe20f3cb227f8a4ed03297940275395efcebc0c7
Bas Wegh <bas.wegh@kit.edu>
2017-01-11 14:45:40 +0100

    rename tts_file_util -> watts_file_util

--- test/tts_file_util_test.erl
+++ test/tts_file_util_test.erl
@@ -19,43 +19,43 @@ absolute_path_test() ->
     Exp7 = "/home/tts/.config/tts.config",
     Exp8 = <<"/home/tts/.config/tts.config">>,
 
-    Res1  = tts_file_util:to_abs("tts.config"),
+    Res1  = watts_file_util:to_abs("tts.config"),
     ?assertEqual(Exp1, Res1),
 
-    Res2 = tts_file_util:to_abs("tts.config", "/etc/tts"),
+    Res2 = watts_file_util:to_abs("tts.config", "/etc/tts"),
     ?assertEqual(Exp2, Res2),
 
-    Res3 = tts_file_util:to_abs("tts.config", <<"/etc/tts">>),
+    Res3 = watts_file_util:to_abs("tts.config", <<"/etc/tts">>),
     ?assertEqual(Exp3, Res3),
 
 
-    Res4_1 = tts_file_util:to_abs(<<"tts.config">>),
+    Res4_1 = watts_file_util:to_abs(<<"tts.config">>),
     ?assertEqual(Exp4_1, Res4_1),
-    Res4_2 = tts_file_util:to_abs(<<"tts.config">>, <<"/etc/tts">>),
+    Res4_2 = watts_file_util:to_abs(<<"tts.config">>, <<"/etc/tts">>),
     ?assertEqual(Exp4_2, Res4_2),
-    Res4_3 = tts_file_util:to_abs(<<"tts.config">>, "/etc/tts"),
+    Res4_3 = watts_file_util:to_abs(<<"tts.config">>, "/etc/tts"),
     ?assertEqual(Exp4_3, Res4_3),
 
-    Res5_1 = tts_file_util:to_abs(<<"~/.config/tts">>),
+    Res5_1 = watts_file_util:to_abs(<<"~/.config/tts">>),
     ?assertEqual(Exp5, Res5_1),
-    Res5_2 = tts_file_util:to_abs(<<"~/.config/tts">>, <<"/etc/tts">>),
+    Res5_2 = watts_file_util:to_abs(<<"~/.config/tts">>, <<"/etc/tts">>),
     ?assertEqual(Exp5, Res5_2),
-    Res6_1 = tts_file_util:to_abs("~/.config/tts"),
+    Res6_1 = watts_file_util:to_abs("~/.config/tts"),
     ?assertEqual(Exp6, Res6_1),
-    Res6_2 = tts_file_util:to_abs("~/.config/tts", "/etc/tts"),
+    Res6_2 = watts_file_util:to_abs("~/.config/tts", "/etc/tts"),
     ?assertEqual(Exp6, Res6_2),
 
     AbsPath = "/home/tts/.config/tts.config",
-    Res7_1 = tts_file_util:to_abs(AbsPath),
+    Res7_1 = watts_file_util:to_abs(AbsPath),
     ?assertEqual(Exp7, Res7_1),
-    Res7_2 = tts_file_util:to_abs(AbsPath, "/etc/tts"),
+    Res7_2 = watts_file_util:to_abs(AbsPath, "/etc/tts"),
     ?assertEqual(Exp7, Res7_2),
 
 
     AbsBin = <<"/home/tts/.config/tts.config">>,
-    Res8_1 = tts_file_util:to_abs(AbsBin),
+    Res8_1 = watts_file_util:to_abs(AbsBin),
     ?assertEqual(Exp8, Res8_1),
-    Res8_2 = tts_file_util:to_abs(AbsBin, "/etc/tts"),
+    Res8_2 = watts_file_util:to_abs(AbsBin, "/etc/tts"),
     ?assertEqual(Exp8, Res8_2),
 
     ok = ?UNSETCONFIG(config_path),
