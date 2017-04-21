55de2d19a1086d3ad5ceff798d7ecc24cbe78c1a
Bas Wegh <bas.wegh@kit.edu>
2017-04-21 14:40:09 +0200

    reorganize code and clean up

--- src/watts_http_rsp.erl
+++ src/watts_http_rsp.erl
@@ -24,38 +24,49 @@ setup_session_and_start({ok, Rsp}, Req) ->
     {ok, Session} = watts:session_for_rsp(Rsp),
     execute_or_error(watts_rsp:request_type(Rsp), Session, Rsp, Req);
 setup_session_and_start(Error, Req) ->
-    lager:warning("RST: failed due to ~p", [Error]),
+    warning("~p", [Error]),
     {ok, Req2} = cowboy_req:reply(400, Req),
     {ok, Req2, []}.
 
 
 execute_or_error(rsp_no_ui_no_login, Session, Rsp, Req) ->
-    {Iss, Sub} = watts_rsp:get_iss_sub(Rsp),
-    ok = watts_session:set_iss_sub(Iss, Sub, Session),
-    %% todo: trigger service
+    {ok, Req2} = login_with_rsp(Session, Rsp, Req),
+    {ServiceId, Params} = watts_rsp:get_service_data(Rsp),
+    watts:request_credential_for(ServiceId, Session, Params),
     Url = watts_rsp:get_return_url(Rsp),
-    watts_http_util:redirect_to(Url, Req);
-execute_or_error(rsp_no_ui_with_login, Session, Rsp, Req) ->
-    Provider = watts_rsp:get_provider(Rsp),
-    Path = io_lib:format("oidc?provider=~s", [binary_to_list(Provider)]),
-    Url = watts_http_util:relative_path(Path),
-    {ok, Max} = watts_session:get_max_age(Session),
-    {ok, Token} = watts_session:get_sess_token(Session),
-    {ok, Req2} = watts_http_util:perform_cookie_action(update, Max, Token, Req),
     watts_http_util:redirect_to(Url, Req2);
 execute_or_error(rsp_with_ui_no_login, Session, Rsp, Req) ->
-    lager:warning("rsp request failed with not yet supported: ~p",[Rsp]),
-    watts:logout(Session),
-    {ok, Req2} = cowboy_req:reply(400, Req),
-    {ok, Req2, []};
+    {ok, Req2} = login_with_rsp(Session, Rsp, Req),
+    Url = watts_http_util:relative_path(""),
+    watts_http_util:redirect_to(Url, Req2);
+execute_or_error(rsp_no_ui_with_login, Session, Rsp, Req) ->
+    redirect_to_provider(Session, Rsp, Req);
 execute_or_error(rsp_with_ui_with_login, Session, Rsp, Req) ->
-    lager:warning("rsp request failed with not yet supported: ~p",[Rsp]),
-    watts:logout(Session),
-    {ok, Req2} = cowboy_req:reply(400, Req),
-    {ok, Req2, []};
-execute_or_error({error, Reason}, Session, Rsp, Req) ->
+    redirect_to_provider(Session, Rsp, Req);
+execute_or_error({error, Reason}, Session, _Rsp, Req) ->
     % bad request type
-    lager:warning("rsp request failed with ~p: ~p",[Reason, Rsp]),
+    warning("~p",[Reason]),
     watts:logout(Session),
     {ok, Req2} = cowboy_req:reply(400, Req),
     {ok, Req2, []}.
+
+login_with_rsp(Session, Rsp, Req) ->
+    watts:login_with_rsp(Rsp, Session),
+    set_cookie(Session, Req).
+
+redirect_to_provider(Session, Rsp, Req) ->
+    {ok, Req2} = set_cookie(Session, Req),
+    Provider = watts_rsp:get_provider(Rsp),
+    Path = io_lib:format("oidc?provider=~s", [binary_to_list(Provider)]),
+    Url = watts_http_util:relative_path(Path),
+    watts_http_util:redirect_to(Url, Req2).
+
+set_cookie(Session, Req) ->
+    {ok, Max} = watts_session:get_max_age(Session),
+    {ok, Token} = watts_session:get_sess_token(Session),
+    watts_http_util:perform_cookie_action(update, Max, Token, Req).
+
+
+warning(Msg, Params) ->
+    Message = io_lib:format(Msg, Params),
+    lager:warning("rsp request failed with ~s", [Message]).
