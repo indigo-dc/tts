171e1074526fd2fbcb7367854db4d29dc06586b1
Bas Wegh <bas.wegh@kit.edu>
2017-06-27 07:39:59 +0200

    adjust oidc client tests

--- test/watts_oidc_client_test.erl
+++ test/watts_oidc_client_test.erl
@@ -4,8 +4,8 @@
 succeed_test() ->
     {ok, Meck} = start_meck(),
     try
-        {ok, _} = watts_oidc_client:login_succeeded(#{token => bad}),
-        {ok, _} = watts_oidc_client:login_succeeded(#{token => good})
+        {ok, _} = watts_oidc_client:login_succeeded(#{token => bad}, type),
+        {ok, _} = watts_oidc_client:login_succeeded(#{token => good}, type)
     after
         ok = stop_meck(Meck)
     end,
@@ -17,12 +17,13 @@ succeed_test() ->
 fail_test() ->
     {ok, Meck} = start_meck(),
     try
-        {ok, _} = watts_oidc_client:login_failed(internal, {token_invalid, blah}),
-        {ok, _} = watts_oidc_client:login_failed(internal, {bad_user_agent, blah}),
-        {ok, _} = watts_oidc_client:login_failed(internal, {bad_peer_ip, blah}),
-        {ok, _} = watts_oidc_client:login_failed(internal, {bad_cookie, blah}),
-        {ok, _} = watts_oidc_client:login_failed(session_not_found,  blah),
-        {ok, _} = watts_oidc_client:login_failed(blah,  blah)
+        EnvMap = #{req => req},
+        {ok, _} = watts_oidc_client:login_failed(internal, {token_invalid, blah}, EnvMap),
+        {ok, _} = watts_oidc_client:login_failed(internal, {bad_user_agent, blah}, EnvMap),
+        {ok, _} = watts_oidc_client:login_failed(internal, {bad_peer_ip, blah}, EnvMap),
+        {ok, _} = watts_oidc_client:login_failed(internal, {bad_cookie, blah}, EnvMap),
+        {ok, _} = watts_oidc_client:login_failed(session_not_found,  blah, EnvMap),
+        {ok, _} = watts_oidc_client:login_failed(blah,  blah, EnvMap)
     after
         ok = stop_meck(Meck)
     end,
@@ -30,12 +31,14 @@ fail_test() ->
 
 
 start_meck() ->
-    MeckModules = [watts, watts_session_mgr],
+    MeckModules = [watts, watts_session_mgr, cowboy_req],
     {ok, Pid1} = watts_session:start_link(<<"1">>),
     ok = watts_session:set_token(#{}, Pid1),
+    ok = watts_session:set_type(oidc, Pid1),
     {ok, Pid2} = watts_session:start_link(<<"2">>),
     ok = watts_session:set_token(#{}, Pid2),
-    Login = fun(TokenMap) ->
+    ok = watts_session:set_type({rsp, login, ui}, Pid2),
+    Login = fun(TokenMap, _) ->
                     case TokenMap of
                         #{token := good} ->
                             {ok, #{session_pid => Pid1, session_type => oidc}};
@@ -46,10 +49,16 @@ start_meck() ->
     ErrorSession = fun(_Msg) ->
                            {ok, Pid2}
                    end,
+    ReturnSession = fun(_) ->
+                            {ok, Pid1}
+                    end,
+    Cookies = fun(Req) -> {[{watts_http_util:cookie_name(), <<"data">>}], Req} end,
     ok = test_util:meck_new(MeckModules),
     ok = meck:expect(watts, login_with_oidcc, Login),
     ok = meck:expect(watts, session_with_error, ErrorSession),
     ok = meck:expect(watts_session_mgr, session_terminating, fun(_) -> ok end),
+    ok = meck:expect(watts_session_mgr, get_session, ReturnSession),
+    ok = meck:expect(cowboy_req, cookies, Cookies),
     {ok, {MeckModules, Pid1, Pid2}}.
 
 
