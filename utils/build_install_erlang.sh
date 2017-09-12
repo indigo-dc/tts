1896b4a728f8567daf4bc59e04adcc59190c920f
Bas Wegh <bas.wegh@kit.edu>
2017-09-12 14:55:20 +0200

    enhance fetching erlang, fix #456

--- utils/build_install_erlang.sh
+++ utils/build_install_erlang.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-VERSION=20.0
+VERSION=20.0.4
 cd /tmp
-wget --progress=bar http://erlang.org/download/otp_src_$VERSION.tar.gz
-tar -xzf otp_src_$VERSION.tar.gz
-rm otp_src_$VERSION.tar.gz
-cd otp_src_$VERSION
-./configure --with-ssl --enable-builtin-zlib --enable-plain-emulator --without-wx --without-jinterface --without-odbc
+
+# http://erlang.org/download/otp_src_$VERSION.tar.gz
+OTPNAME=OTP-$VERSION
+
+rm -f $OTPNAME.tar.gz
+wget --progress=bar https://github.com/erlang/otp/archive/$OTPNAME.tar.gz
+tar -xzf $OTPNAME.tar.gz
+rm $OTPNAME.tar.gz
+cd otp-$OTPNAME
+./otp_build autoconf
+./configure --with-ssl --enable-builtin-zlib --without-wx --without-jinterface --without-odbc --without-debugger --without-observer --without-et
 make
 sudo make install
 cd ..
-rm -rf otp_src_$VERSION
+rm -rf otp-$OTPNAME
