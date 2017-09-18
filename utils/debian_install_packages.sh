81efd0c3d9e0f8de83ab64111d9e4a8e84c86a6d
Bas Wegh <bas.wegh@kit.edu>
2017-09-18 10:57:11 +0200

    ensure autoconf is available

--- utils/debian_install_packages.sh
+++ utils/debian_install_packages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get -y install wget libncurses5-dev libncursesw5-dev libssl-dev build-essential
+sudo apt-get -y install wget libncurses5-dev libncursesw5-dev libssl-dev build-essential autoconf
