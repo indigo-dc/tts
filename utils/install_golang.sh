2c1a5e3263c9ab08203cb82b0dd483acb837fc64
Bas Wegh <bas.wegh@kit.edu>
2017-12-05 14:32:07 +0100

    add script to install newer golang version

---
+++ utils/install_golang.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd `dirname $0`
+UTILS_DIR=`pwd`
+
+cd $UTILS_DIR/..
+mkdir -p _build/tmp/go
+cd _build/tmp/go
+
+curl -O https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
+tar -xzf go1.8.3.linux-amd64.tar.gz
+sudo mv go /usr/local
+cd ..
+rm -rf go
+export PATH="/usr/local/go/bin:$PATH"
+
+echo -n "go version: "
+go version
