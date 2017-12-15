f1e824e4bdf6d3ffc8f7993d0c02d4cd9f8cb780
Bas Wegh <bas.wegh@kit.edu>
2017-12-15 13:47:07 +0100

    delete old golang before copying

--- utils/install_golang.sh
+++ utils/install_golang.sh
@@ -3,6 +3,16 @@ cd `dirname $0`
 UTILS_DIR=`pwd`
 GO=`which go`
 
+echo "installing:"
+echo "   _____  ____  _                   ";
+echo "  / ____|/ __ \| |                  ";
+echo " | |  __| |  | | | __ _ _ __   __ _ ";
+echo " | | |_ | |  | | |/ _\` | '_ \ / _\` |";
+echo " | |__| | |__| | | (_| | | | | (_| |";
+echo "  \_____|\____/|_|\__,_|_| |_|\__, |";
+echo "                               __/ |";
+echo "                              |___/ ";
+
 echo "GO = $GO"
 ls -al $GO
 if [ "x" == "x$GO" ]; then
@@ -28,15 +38,16 @@ elif [ -f $GO ]; then
     GODIR=`cd $GODIR/.. && pwd`
 fi
 echo "GODIR = $GODIR"
+sudo rm -rf $GODIR/*
+
 
 cd $UTILS_DIR/..
 mkdir -p _build/tmp/go
 cd _build/tmp/go
-
-wget --progress=bar https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
 tar -xzf go1.8.3.linux-amd64.tar.gz
 cd go
-sudo cp -r * $GODIR
+sudo cp -vr * $GODIR
 cd ..
 rm -rf go
 
